ae3d8380bbe1c7977e671245f7bd9d7f49487d5c
Karan Singh Parmar <karanparmar0130@gmail.com>
2024-07-21 17:17:22 +0100

    Stacks using array

---
+++ Stack/ArrayStack.cs
@@ -0,0 +1,68 @@
+namespace Stack;
+
+public class ArrayStack(int size)
+{
+    // Initial values
+    private int top = -1; // Postion of top most element on stack
+    private readonly int maxSize = size; // Total size of stack
+    private readonly int[] stack = new int[size];
+
+    // Operations
+    public void Push(int value)
+    {
+        // Don't do anything if stack is full
+        if (top == maxSize -1)
+        {
+            Console.WriteLine("Stack overflow");
+            return;
+        }
+
+        stack[top+1] = value;
+        Console.WriteLine($"Value pushed {stack[top+1]}"); 
+
+        top += 1; // Increase top by 1
+    }
+
+    public void Pop()
+    {
+        // Don't do anything if stack is empty
+        if (top == -1)
+        {
+            Console.WriteLine("Stack underflow");
+            return;
+        }
+
+        Console.WriteLine($"Value popped {stack[top]}"); 
+
+        top -= 1; // Decrease top by 1
+    }
+
+    public int Peek()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack is empty"); 
+            return -1;
+        }
+        
+        return stack[top];
+    }
+
+    public void Print()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack is empty");    
+            return;
+        }
+
+        var printValue = string.Empty;
+
+        for(int i=0; i<= top; i++)
+        {
+            printValue += stack[i] + " ";
+        }
+        
+        Console.WriteLine(printValue);    
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,42 @@
+﻿using Stack;
+
+var stack = new ArrayStack(3);
+
+// Should print empty stack
+stack.Print();
+
+// Should print empty stack
+stack.Peek();
+
+stack.Push(1);
+stack.Push(5);
+stack.Push(9);
+
+// Top element should be 9
+stack.Peek();
+
+// Should print 1 5 9
+stack.Print();
+
+// Stack Overflow
+stack.Push(2);
+
+stack.Pop();
+
+// Top element should be 5
+stack.Peek();
+
+// Should print 1 5
+stack.Print();
+
+stack.Pop();
+stack.Pop();
+
+// Stack underflow
+stack.Pop();
+
+// Should print empty stack
+stack.Peek();
+
+// Should print empty stack
+stack.Print();
